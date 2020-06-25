30cd370a09466deb8c4a2c8e4236cbff7927e95a
Atem <arkobelkov@ya.ru>
2020-06-26 02:29:15 +0300

    Добавил адаптацию под все размеры, закончил  первую страницу (home)

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,20 @@ $(document).ready(function(){
 		]
 	});
 });
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.home__nav'),
+    menuItem = document.querySelectorAll('.home__item'),
+    hamburger = document.querySelector('.home__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('home__hamburger_active');
+        menu.classList.toggle('home__nav_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('home__hamburger_active');
+            menu.classList.toggle('home__nav_active');
+        })
+    })
+})
