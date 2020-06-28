d47c039b9ce8c64b8b97e10ba883b341958a84ea
Atem <arkobelkov@ya.ru>
2020-06-28 22:28:40 +0300

    Создал страницу "Портфолио"

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.home__nav'),
+    menuItem = document.querySelectorAll('.home__item'),
+    hamburger = document.querySelector('.home__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('home__hamburger_active');
+        menu.classList.toggle('home__nav_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('home__hamburger_active');
+            menu.classList.toggle('home__nav_active');
+        })
+    })
+})
 $(document).ready(function(){
 	$('.home__carousel').slick({
 		dots: true,
@@ -18,20 +35,3 @@ $(document).ready(function(){
 		]
 	});
 });
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.home__nav'),
-    menuItem = document.querySelectorAll('.home__item'),
-    hamburger = document.querySelector('.home__hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('home__hamburger_active');
-        menu.classList.toggle('home__nav_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('home__hamburger_active');
-            menu.classList.toggle('home__nav_active');
-        })
-    })
-})
